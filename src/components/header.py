305a00e00df5c87e5d11d4d82290c2bb2f1493a0
Agustin Castro <agustin.castro.91@gmail.com>
2019-01-18 14:12:58 -0300

    fixed title css

--- src/components/header.py
+++ src/components/header.py
@@ -44,25 +44,18 @@ headerComponent_configuration = html.Div(
                         'color': '#4D637F'
                     }),
             html.Hr(),
-        ], className='row twelve columns', style={'position': 'relative', 'right': '15px'}),
+        ], className='row twelve columns header-container'),
 
         html.Div([
-            html.H2(
+            html.P(
                 '''
             IDMINER is a tool that allows you to explore terms associated with query genes.
-            The terms are extracted from articles related to the query gene obtained from parsing [PaperBLAST](http://papers.genomics.lbl.gov/cgi-bin/litSearch.cgi) tool.
+            The terms are extracted from articles related to the query gene obtained from 
+            parsing PaperBLAST tool.
             ''',
-            style={
-                'position': 'relative',
-                'top': '0px',
-                'left': '25px',
-                'font-family': 'Dosis',
-                'display': 'inline',
-                'font-size': '2.0rem',
-                'color': '#000000'
-                }
+                className='header-subtext'
             )
-                ])
+        ])
     ])
 
 
@@ -109,23 +102,14 @@ headerComponent_dashboard = html.Div(
                         'color': '#4D637F'
                     }),
             html.Hr(),
-        ], className='row twelve columns', style={'position': 'relative', 'right': '15px'}),
+        ], className='row twelve columns header-container', style={'position': 'relative', 'right': '15px'}),
 
-        html.Div([            
-            html.H2(
+        html.Div([
+            html.P(
                 '''
             Discover relevant terms in your set of study genes through the exploration of the most relevant terms in the set of abstracts where the study genes (or homologs) are mentioned.
-
             ''',
-            style={
-                'position': 'relative',
-                'top': '0px',
-                'left': '25px',
-                'font-family': 'Dosis',
-                'display': 'inline',
-                'font-size': '2.0rem',
-                'color': '#000000'
-                }
+                className='header-subtext'
             ),
             html.Hr(),
         ])
@@ -165,24 +149,15 @@ headerComponent_geneboard = html.Div(
                         'color': '#4D637F'
                     }),
             html.Hr(),
-        ], className='row twelve columns', style={'position': 'relative', 'right': '15px'}),
+        ], className='row twelve columns header-container'),
 
         html.Div([
-            
-            html.H2(
+
+            html.P(
                 '''
             In this section you can explore the relationships between your query genes. In the table you can see how many articles in common have two genes given. While in the network you can select all the articles that are related to the query gene, and also, selecting a node (from the network) you can see the articles that have in common the selected node (subject gene) and the query gene.
-
             ''',
-            style={
-                'position': 'relative',
-                'top': '0px',
-                'left': '25px',
-                'font-family': 'Dosis',
-                'display': 'inline',
-                'font-size': '2.0rem',
-                'color': '#000000'
-                }
+                className='header-subtext'
             ),
             html.Hr(),
         ])
